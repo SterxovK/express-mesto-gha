8a3d06eee1162b1a74b3fd921e6a12e5cf7795b0
Kostya Sterkhov <sterxovk@mail.ru>
2022-06-12 13:38:34 +0400

    fix/bag

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 const User = require("../models/user");
 
-const getUsers = (res) => {
+const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
     .catch((err) => {
