c247996580bd9bea7b19c13802bce6f976ec32be
Kostya Sterkhov <sterxovk@mail.ru>
2022-06-19 21:56:03 +0400

    fix joi

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards,
   deleteCard,
@@ -8,9 +9,42 @@ const {
 } = require('../controllers/cards');
 
 router.get('/', getCards); // получить все карточки
-router.post('/', createCard); // создать карточку
-router.delete('/:cardId', deleteCard); // удалить карточку
-router.put('/:cardId/likes', likeCard); // поставить лайк карточке
-router.delete('/:cardId/likes', dislikeCard); // убрать лайк с карточки
+router.post(
+  '/',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      link: Joi.string().min(2),
+    }),
+  }),
+  createCard,
+); // создать карточку
+router.delete(
+  '/:cardId',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().length(24).hex(),
+    }),
+  }),
+  deleteCard,
+); // удалить карточку
+router.put(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().length(24).hex(),
+    }),
+  }),
+  likeCard,
+); // поставить лайк карточке
+router.delete(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().length(24).hex(),
+    }),
+  }),
+  dislikeCard,
+); // убрать лайк с карточки
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers,
   getUser,
@@ -8,9 +9,22 @@ const {
 } = require('../controllers/users');
 
 router.get('/', getUsers); // получить всех юзеров
-router.get('/:userId', getUser); // получить юзера по id
-router.get('/me', getUserMe); // обновляет профиль
-router.patch('/me', updateUser); // обновляет профиль
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex(),
+  }),
+}), getUser); // получить юзера по id
+router.get('/me', getUserMe); // получить свой профиль
+router.patch(
+  '/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+    }),
+  }),
+  updateUser,
+); // обновляет профиль
 router.patch('/me/avatar', updateAvatar); // обновляет аватар
 
 module.exports = router;
