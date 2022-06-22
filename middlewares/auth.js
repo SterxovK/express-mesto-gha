040bc882b62f545f75b80069843c58b2c93fba84
Kostya Sterkhov <sterxovk@mail.ru>
2022-06-22 14:53:13 +0400

    fix auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,18 +8,18 @@ const auth = (req, res, next) => {
   // eslint-disable-next-line prefer-destructuring
   const cookies = req.cookies;
   if (!cookies) {
-    return res.status(401).send({ error: 'авторизация неуспешна' });
+    return res.status(401).send({ messege: 'авторизация неуспешна' });
   }
   const token = cookies.jwt;
   if (!token) {
-    return res.status(401).send({ error: 'авторизация неуспешна' });
+    return res.status(401).send({ messege: 'авторизация неуспешна' });
   }
   let payload;
   try {
     payload = jwt.verify(token, 'secret-key');
   } catch (err) {
     console.log('fer');
-    return res.status(403).send({ error: 'jwt token is not valid' });
+    return res.status(403).send({ messege: 'jwt token is not valid' });
   }
   req.user = payload;
   next();
